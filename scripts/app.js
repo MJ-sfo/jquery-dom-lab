4a73ea413f9a023676c5b9125bb38c4add1889d0
MJ-sfo <michaeljlaird@yahoo.com>
2017-03-30 22:26:53 -0700

    adds header and class

--- scripts/app.js
+++ scripts/app.js
@@ -10,9 +10,15 @@ $( document ).ready(function whenLoaded(){
   $('#greeting').text("Hola");
 
   // 3. Add an h1 to the page that says "A Spanish Greeting".
+  $( ".col-sm-6" ).append( "<h1>A Spanish Greeting</h1>" );
 
   // 4. Add a second greeting ("Que tal?"), in a second column to the right of the first.
+  //  sorry, not enough time - ml
 
   // 5. Add the class "blue" to both greetings.
+  
+  $( "h1" ).addClass( "blue" );
+  $("h1").css("color", "blue");
+  $(".blue").css("color", "green");
 
 });
